2c7c9371a3d120fde794b51f32813adefa744a9e
James HR <jaimehr2101@gmail.com>
2024-11-17 17:25:38 -0600

    feat(main): agregar cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   // Validators
   app.useGlobalPipes(
     new ValidationPipe({
